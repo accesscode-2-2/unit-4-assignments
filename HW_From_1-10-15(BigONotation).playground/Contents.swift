9722f9981087b52aba8cb191e92334f9e396af11
benstone1 <benstone1@github.com>
2016-01-11 13:04:06 -0500

    added 2 hw submission playgrounds

---
+++ HW_From_1-10-15(BigONotation).playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+
+Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
+
+https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
+
+
+1)
+
+2)
+
+3)
+
+4)
+
+5)
+
+6)
+
+7)
+
+
+*/
+
+
---
+++ HWfrom1-09-16(SwiftIntro).playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+
+Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
+
+https://docs.google.com/document/d/1DQ2aCJ_yUZtazzCfb0PaS81bg61V2ZOSxpABh981xSo/edit
+
+
+1)
+
+2)
+
+3)
+
+4)
+
+
+*/
