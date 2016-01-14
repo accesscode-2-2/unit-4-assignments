e230e37a1e64e0b10db8be20f8e9dfd17f0da0e4
benstone1 <benstone1@github.com>
2016-01-14 17:43:27 -0500

    added assignment from 1-14;

---
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+/*
+
+Question 1: https://www.hackerrank.com/challenges/minimum-draws
+
+Copy and paste your code:
+
+What is the big O runtime of your code?:
+
+Question 2: https://www.hackerrank.com/challenges/handshake
+
+Copy and paste your code:
+
+What is the big O runtime of your code?:
+
+Question 3: https://www.hackerrank.com/challenges/connecting-towns
+
+Copy and paste your code:
+
+What is the big O runtime of your code?:
+
+*/
+
