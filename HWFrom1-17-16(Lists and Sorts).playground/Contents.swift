cde78394103ad73b556b455ae51bbdb81151cc7c
benstone1 <benstone1@github.com>
2016-01-18 12:01:44 -0500

    added 1-17 hw

---
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+
+
+Work on your solutions here.
+
+Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB7fDkg/edit#heading=h.uopysoy45zmw
+
+1) 
+
+
+
+2) 
+
+
+
+3)
+
+
+
+*/
