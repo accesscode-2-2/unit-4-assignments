38eac0e09074171b6ee04fb720bb3f14855c4b0b
alizinha <benjamin.h.stone@gmail.com>
2016-01-30 17:04:00 -0500

    added hw from 1-30-16

---
+++ HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+//Answer the questions in the homework below
+//https://docs.google.com/document/d/1KlK3PmRMybmHS5db_AP11uHXIcbIeM5vHuuAZOV3xME/edit#
+
+//1)
+
+//2)
+
+//3)
+
+//4)
+
+//5)
+
+//6)
